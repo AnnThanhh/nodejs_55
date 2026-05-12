cfb28dec43bfa840fb93278a30d60c0a7a7321cd
AnnThanhh <trinhanthanh@gmail.com>
2026-05-12 22:01:07 +0700

    update buổi 5

--- expressjs/src/models/article.model.js
+++ expressjs/src/models/article.model.js
@@ -26,10 +26,47 @@ const Article = sequelize.define(
       allowNull: false,
       defaultValue: 0,
     },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Users", //đúng chính xác các thông tin trong database
+        key: "id",
+      },
+    },
+    deletedBy: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    isDeleted: {
+      type: DataTypes.BOOLEAN, // tiny (1): chỉ chứa 2 giá trị: 0 và số 1
+      defaultValue: 0, // 0: false, 1: true
+    },
+    deletedAt: {
+      type: "TIMESTAMP",
+      defaultValue: null,
+      allowNull: true,
+    },
+    createdAt: {
+      type: "TIMESTAMP",
+      allowNull: false,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"), //giá trị mặc định sẽ là thời gian hiện tại khi bản ghi được tạo ra trong database
+    },
+    updatedAt: {
+      type: "TIMESTAMP",
+      allowNull: false,
+      defaultValue: Sequelize.literal(
+        "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
+      ),
+    },
   },
   {
-   tableName: "Articles", //tên bảng trong database, nếu không có trường này, Sequelize sẽ tự động chuyển tên modal thành chữ thường và thêm "s" vào cuối để làm tên bảng (ví dụ: Article -> articles)
+    tableName: "Articles", //tên bảng trong database, nếu không có trường này, Sequelize sẽ tự động chuyển tên modal thành chữ thường và thêm "s" vào cuối để làm tên bảng (ví dụ: Article -> articles)
+    timestamps: false, //tắt tính năng tự động thêm 2 cột createdAt và updatedAt vào bảng trong database
   },
 );
+
+//code first:
+await Article.sync(); //đồng bộ modal Article với bảng Articles trong database, nếu bảng chưa tồn tại sẽ được tạo mới, nếu đã tồn tại sẽ không làm gì cả
 
 export default Article;
