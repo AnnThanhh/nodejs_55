344fe77ac1dd236eca5318998048b5338c5adddc
AnnThanhh <trinhanthanh@gmail.com>
2026-05-14 21:37:10 +0700

    update buổi 6

--- expressjs/src/controllers/article.controller.js
+++ expressjs/src/controllers/article.controller.js
@@ -1,9 +1,20 @@
+import { responseSuccess } from "../common/helpers/response.helper.js";
 import { articleService } from "../services/article.service.js";
 //controller: sử dụng để gọi/điều hướng qua các service tương ứng, sau đó trả về response cho client
 export const articleController = {
-  async findAll(request, response, next) {
+  async findAll(req, res, next) {
     //gọi service
     const articles = await articleService.findAll();
-    response.json(articles); // trả về cho client dưới dạng json
+    const response = responseSuccess(
+      articles,
+      "Lấy danh sách Article thành công",
+    );
+    res.json(response); // trả về cho client dưới dạng json
+  },
+
+  async create(req, res, next) {
+    const articles = await articleService.create(req);
+    const response = responseSuccess(articles, "Tạo Article thành công");
+    res.json(response); // trả về cho client dưới dạng json
   },
 };
--- expressjs/src/services/article.service.js
+++ expressjs/src/services/article.service.js
@@ -1,12 +1,29 @@
 //service: xử lý logic nghiệp vụ, tương tác với database, sau đó trả về kết quả cho controller
-import sequelize from "../common/squelize/connect.sequelize.js";
+// import sequelize from "../common/squelize/connect.sequelize.js";
 import Article from "../models/article.model.js";
 import { prisma } from "../common/prisma/connect.prisma.js";
 
-
 export const articleService = {
   async findAll() {
-    const res = await Article.findAll();
+    //squelize
+    // const res = await Article.findAll();
+
+    //prisma
+    const res = await prisma.articles.findMany();
     return res;
+  },
+
+  async create(req) {
+    const body = req.body;
+
+    const res = await prisma.articles.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        userId: body.userId,
+      },
+    });
+
+    return true;
   },
 };
